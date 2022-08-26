2bb6573ea26bf6ef4d9a822a57218a2cf22787cd
harpriyabagri <harpriya@paper.xyz>
2022-08-25 17:30:18 -0700

    new github link

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Footer() {
-  const url = "https://github.com/thirdweb-example/edition-drop";
+  const url = "https://github.com/paperxyz/workshops";
   return (
     <>
       <div
